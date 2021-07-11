20c9fe14d0c67522215ce2f24e9c059726714b06
Huy <lqhuy.1320@gmail.com>
2021-07-11 16:52:15 +0700

    afasf

--- QLDA/QLDA/assets/dist/js/pages/Project.js
+++ QLDA/QLDA/assets/dist/js/pages/Project.js
@@ -1,1 +1,37 @@
-﻿
+﻿var common = {
+    init: function () {
+        common.registerEvent();
+    },
+    registerEvent: function () {
+        $("#txtkeyword").autocomplete({
+            minLength: 0,
+            source: function (request, response) {
+                $.ajax({
+                    url: "/Student/listName",
+                    dataType: "json",
+                    data: {
+                        q: request.term
+                    },
+                    success: function (res) {
+                        response(res.data);
+                    }
+                });
+            },
+            focus: function (event, ui) {
+                $("#txtkeyword").val(ui.item.label);
+                return false;
+            },
+            select: function (event, ui) {
+                $("#txtkeyword").val(ui.item.label);
+
+                return false;
+            }
+        })
+            .autocomplete("instance")._renderItem = function (ul, item) {
+                return $("<li>")
+                    .append("<div>" + item.label + "</div>")
+                    .appendTo(ul);
+            };
+    }
+}
+common.init();
